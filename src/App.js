ff82902dfffe74a1f6ce206b8a22459ced3ab033
Aravind071845 <727721euec011@skcet.ac.in>
2023-03-22 21:33:59 +0530

    msg

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Reference Data Types use same memory</h2>
+      <button onClick={window['button']}>click here</button>
+    </div>
+  );
+}
+
+export default App;
